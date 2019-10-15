be7a6cc29e3db299c2b5f2a5f3eb8668092bb146
John Kastner <john.h.kastner@gmail.com>
2019-10-15 10:00:24 -0400

    Add __main__ guard

--- equal_breaks.py
+++ equal_breaks.py
@@ -161,20 +161,16 @@ def dpOptimalEqualAreaBreaks(numbers, k):
 
     return best_breaks[len(numbers)][k - 1]
 
-world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
-world = world[(world.pop_est>0) & (world.name!="Antarctica")]
-world = world.to_crs({'proj': 'wintri'})
-world['area'] = world.geometry.area
+if __name__ == '__main__':
+    world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+    world = world[(world.pop_est>0) & (world.name!="Antarctica")]
+    world = world.to_crs({'proj': 'wintri'})
+    world['area'] = world.geometry.area
 
-fig, axes = plt.subplots(nrows = 2, ncols = 2)
+    fig, axes = plt.subplots(nrows = 2, ncols = 2)
 
-world.plot(ax=axes[0,0], edgecolor='black', cmap='OrRd', column='pop_est', scheme='quantiles')
-world.plot(ax=axes[0,1], edgecolor='black', cmap='OrRd', column='pop_est', scheme='equal_area_greedy', classification_kwds={'area': world.area})
-world.plot(ax=axes[1,0], edgecolor='black', cmap='OrRd', column='pop_est', scheme='equal_area_greedy2', classification_kwds={'area': world.area})
-world.plot(ax=axes[1,1], edgecolor='black', cmap='OrRd', column='pop_est', scheme='equal_area_dp', classification_kwds={'area': world.area})
-plt.show()
-
-#area = [10, 2, 2, 10, 12, 12]
-#data = [1, 2, 3, 4, 5, 6]
-#for k in range(1, len(area)):
-#    print(Equal_Area_DP(data, area=area, k=k))
+    world.plot(ax=axes[0,0], edgecolor='black', cmap='OrRd', column='pop_est', scheme='quantiles')
+    world.plot(ax=axes[0,1], edgecolor='black', cmap='OrRd', column='pop_est', scheme='equal_area_greedy', classification_kwds={'area': world.area})
+    world.plot(ax=axes[1,0], edgecolor='black', cmap='OrRd', column='pop_est', scheme='equal_area_greedy2', classification_kwds={'area': world.area})
+    world.plot(ax=axes[1,1], edgecolor='black', cmap='OrRd', column='pop_est', scheme='equal_area_dp', classification_kwds={'area': world.area})
+    plt.show()
